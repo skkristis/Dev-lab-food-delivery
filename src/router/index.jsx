5fb95cf1663a7cb405f6c131d3474c5b38d96b82
Dovydas Miknys <Dovydas.Miknys@itechart-group.com>
2023-02-21 17:38:07 +0200

    minor routes tweak

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,25 +3,25 @@ import {
   createRoutesFromElements,
   Route,
 } from "react-router-dom/dist";
-import ClientLanding from "../features/client/pages/ClientLanding/index.jsx";
-import AdminLanding from "../features/admin/pages/AdminLanding/index.jsx";
-import ClientLayout from "../features/client/layouts/ClientLayout/index.jsx";
+import ClientLanding from "../features/client/pages/ClientLanding";
+import AdminLanding from "../features/admin/pages/AdminLanding";
+import ClientLayout from "../features/client/layouts/ClientLayout";
 
 function getClientRoutes() {
-  return <Route path="subscriptions" element={<h3>Subscriptions</h3>} />;
+  return [<Route path="subscriptions" element={<h3>Subscriptions</h3>} />];
 }
 
 function getAdminRoutes() {
-  return <Route path="restaurants" element={<h3>Restaurants</h3>} />;
+  return [<Route path="restaurants" element={<h3>Restaurants</h3>} />];
 }
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" errorElement={<ClientLayout />}>
       <Route index element={<ClientLanding />} />
-      {getClientRoutes()}
+      {...getClientRoutes()}
       <Route path="admin" element={<AdminLanding />}>
-        {getAdminRoutes()}
+        {...getAdminRoutes()}
       </Route>
     </Route>
   )
