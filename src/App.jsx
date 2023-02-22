c93ad39897cd0c35dc923ed00acf36f3f62e2fbc
Kristoferis Solovjov <skkristis@gmail.com>
2023-02-22 23:29:35 +0200

    merge-fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom/dist';
 import './App.css';
 import { Provider } from 'react-redux';
 import router from './router/index';
+import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './store/index';
 import { ChakraProvider } from '@chakra-ui/react';
 
