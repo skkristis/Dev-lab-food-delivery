bb7c1564a0e5957c12fdb4bf7d1b4e345dfaac78
Kristoferis Solovjov <skkristis@gmail.com>
2023-02-23 20:58:36 +0200

    basic-setup-internal

--- src/features/client/mocks/restaurantInspectMock.js
+++ src/features/client/mocks/restaurantInspectMock.js
@@ -3,31 +3,81 @@ export const restaurantInspectMock = {
   restaurantAddress: 'Tauro Kalnas',
   restaurantBio: 'Fast food restaurant',
   restaurantRating: '7.5',
+  restaurantWorkingHours: 'Open all day',
   restaurantThumb:
     'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80',
   restaurantMenu: [
+    {
+      sectionName: 'Special Deals!',
+      deals: [
+        {
+          recipeName: 'Burrito',
+          recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
+          recipePrice: '5.4',
+          popular: false,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1574343635105-4cf2ea136b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        },
+        {
+          recipeName: 'Burrito',
+          recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
+          recipePrice: '5.4',
+          popular: true,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1574343635105-4cf2ea136b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        },
+        {
+          recipeName: 'Burrito',
+          recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
+          recipePrice: '5.4',
+          popular: false,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1574343635105-4cf2ea136b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        },
+        {
+          recipeName: 'Burrito',
+          recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
+          recipePrice: '5.4',
+          popular: true,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1574343635105-4cf2ea136b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        },
+      ],
+    },
     {
       sectionName: 'Burritoes',
       deals: [
         {
           recipeName: 'Burrito',
           recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.4',
+          recipePrice: '5.4',
+          popular: false,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1574343635105-4cf2ea136b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
         },
         {
           recipeName: 'Burrito',
           recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.4',
+          recipePrice: '5.4',
+          popular: false,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1574343635105-4cf2ea136b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
         },
         {
           recipeName: 'Burrito',
           recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.4',
+          recipePrice: '5.4',
+          popular: false,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1574343635105-4cf2ea136b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
         },
         {
           recipeName: 'Burrito',
           recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.4',
+          recipePrice: '5.4',
+          popular: false,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1574343635105-4cf2ea136b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
         },
       ],
     },
@@ -37,27 +87,24 @@ export const restaurantInspectMock = {
         {
           recipeName: 'Veggie salad',
           recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.4',
+          recipePrice: '5.4',
+          popular: false,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
         },
         {
           recipeName: 'Veggie salad',
           recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.5',
+          recipePrice: '5.5',
+          recipeThumb:
+            'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
         },
         {
           recipeName: 'Veggie salad',
           recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.2',
-        },
-        {
-          recipeName: 'Veggie salad',
-          recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.4',
-        },
-        {
-          recipeName: 'Veggie salad',
-          recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.4',
+          recipePrice: '5.2',
+          recipeThumb:
+            'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
         },
       ],
     },
@@ -67,17 +114,26 @@ export const restaurantInspectMock = {
         {
           recipeName: 'Chicken burger',
           recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.4',
+          recipePrice: '5.4',
+          popular: false,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80',
         },
         {
           recipeName: 'Hamburger',
           recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.4',
+          recipePrice: '5.4',
+          popular: false,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80',
         },
         {
           recipeName: 'Cheeseburger',
           recipeBio: 'Cucumber, bread, tomatoes, coleslaw, onion, meat, sauce',
-          price: '5.4',
+          recipePrice: '5.4',
+          popular: false,
+          recipeThumb:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80',
         },
       ],
     },
--- src/features/client/pages/RestaurantInspect/index.jsx
+++ src/features/client/pages/RestaurantInspect/index.jsx
@@ -1,7 +1,9 @@
-import { Box, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Heading, Image, Text, Flex, Button } from '@chakra-ui/react';
+
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import './index.css';
+import clockUrl from '../../../../assets/clock-icon.svg';
+import RestaurantInspectSection from '../../components/RestaurantInspectSection';
 
 function RestaurantInspect() {
   const restaurantInfo = useLoaderData();
@@ -14,9 +16,10 @@ function RestaurantInspect() {
           position="absolute"
           left="0"
           right="0"
-          top="-50vw"
+          top={{ base: '0', sm: '-20vw' }}
           zIndex="-2"
-          width="100%"
+          width="100vw"
+          objectFit="cover"
         />
         <Box
           bg="black"
@@ -24,16 +27,56 @@ function RestaurantInspect() {
           position="absolute"
           left="0"
           right="0"
-          top="-50vw"
+          top="-50vh"
           zIndex="-1"
           width="100%"
-          height="100vh"
+          height="200vh"
         ></Box>
 
-        <Box paddingBlock="50px" color="white" paddingInline="30px">
+        <Box
+          className="container"
+          paddingBlock="100px"
+          color="white"
+          paddingInline="30px"
+        >
           <Heading>{`${restaurantInfo.restaurantName} (${restaurantInfo.restaurantAddress})`}</Heading>
           <Text>{restaurantInfo.restaurantBio}</Text>
         </Box>
+      </Box>
+      <Box bg="white" borderBottom="1px solid lightgray">
+        <Flex
+          className="container"
+          alignItems="center"
+          paddingBlock="15px"
+          gap="15px"
+          flexWrap="wrap"
+        >
+          <Flex gap="10px">
+            <Image src={clockUrl} width="15px" />
+            <Text>{restaurantInfo.restaurantWorkingHours}</Text>
+          </Flex>
+          <Box gap="10px">
+            <Text>☆ {restaurantInfo.restaurantRating}</Text>
+          </Box>
+          <Button variant="ghost" color="blue.400" gap="10px">
+            <Box
+              as="span"
+              color="white"
+              padding="1px 9px "
+              bg="blue.400"
+              rounded="xl"
+              marginRight="10px"
+            >
+              i
+            </Box>
+            See more information
+          </Button>
+        </Flex>
+      </Box>
+      <Box bg="white" paddingTop="20px">
+        {restaurantInfo.restaurantMenu.map((dealSection, i) => {
+          return <RestaurantInspectSection key={i} dealSection={dealSection} />;
+        })}
       </Box>
     </Box>
   );
